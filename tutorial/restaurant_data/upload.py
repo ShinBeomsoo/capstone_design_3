eadbfb37532be5e35ec5d467637eab4ed7bf25c1
SHIN BEOMSOO <beomsooshin.leo@gmail.com>
2024-05-20 10:46:56 +0900

    feat: csv file upload

--- tutorial/restaurant_data/upload.py
+++ tutorial/restaurant_data/upload.py
@@ -15,13 +15,14 @@ USER = os.environ["AWS_RDS_USER"]
 DB_NAME = os.environ["AWS_RDS_DB"]
 
 # CSV 파일 읽기
-df = pd.read_csv("./gangnam_restaurant.csv")
+df = pd.read_csv("./normal_restaurant.csv")
 
 # MySQL 데이터베이스에 연결 (여기서 'username', 'password', 'database_name'을 자신의 정보로 바꿔주세요)
 engine = create_engine(
     f"mysql+mysqlconnector://{USER}:{PW}@{ENDPOINT}/{DB_NAME}"
     # host=ENDPOINT, user=USER, password=PW, database="sesame_bot", port=3306
 )
+# restaurant, mobum, measure
 
 # DataFrame을 SQL 테이블로 저장 (여기서 'table_name'을 원하는 테이블 이름으로 바꿔주세요)
-df.to_sql("gangnam_restaurant", con=engine, if_exists="replace", index=False)
+df.to_sql("restaurant", con=engine, if_exists="replace", index=False)
